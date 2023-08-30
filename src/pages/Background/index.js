3ff73ad575e214c87155befe89e34d2f30cc2566
Andrew <96500281+li-andrew@users.noreply.github.com>
2023-08-30 17:56:12 -0400

    on highlight event, print out openai prompt in content console

--- src/pages/Background/index.js
+++ src/pages/Background/index.js
@@ -1,2 +1,9 @@
 console.log('This is the background page.');
 console.log('Put the background scripts here.');
+console.log('sup');
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  if (message.type === 'logText') {
+    console.log(message.text);
+  }
+});
--- src/pages/Content/index.js
+++ src/pages/Content/index.js
@@ -1,6 +1,55 @@
-import { printLine } from './modules/print';
-
 console.log('Content script works!');
-console.log('Must reload extension for modifications to take effect.');
+const { Configuration, OpenAIApi } = require('openai');
 
-printLine("Using the 'printLine' function from the Print Module");
+let openaiClient;
+
+chrome.storage.local.get(['openaiApiKey']).then(({ openaiApiKey }) => {
+  const configuration = new Configuration({
+    apiKey: openaiApiKey,
+  });
+  openaiClient = new OpenAIApi(configuration);
+});
+
+chrome.storage.onChange.addListener((changes) => {
+  for (let [key, { newValue }] of Object.entries(changes)) {
+    if (key !== 'openaiApiKey') continue;
+    openaiClient = new OpenAIApi(
+      new Configuration({
+        apiKey: newValue,
+      })
+    );
+  }
+});
+
+console.log('HELLOOOOOOOO???????????????');
+
+document.addEventListener('mouseup', async (event) => {
+  console.log('where are wee????');
+  //   const hoveredElement = event.target;
+
+  //   if (hoveredElement && hoveredElement.innerText) {
+  //     const hoveredText = hoveredElement.innerText.trim();
+
+  //     if (hoveredText) {
+  //       chrome.runtime.sendMessage({ type: 'logText', text: hoveredText });
+  //     }
+  //   }
+  if (window.getSelection) {
+    // chrome.runtime.sendMessage({
+    //   type: 'logText',
+    //   text: window.getSelection().toString(),
+    // });
+    console.log('r we here?');
+
+    if (!openaiClient) return;
+    console.log('hello');
+    const completion = await openaiClient.createCompletion({
+      model: 'text-davinci-003',
+      prompt:
+        'A user has just highlighted "' +
+        window.getSelection().toString() +
+        '". With this content, please pretend you are a tutor and define/explain the concept/term. Do this using simple terms and as little words as possible.',
+    });
+    console.log(completion.data.choices[0].text);
+  }
+});
